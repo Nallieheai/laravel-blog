0a47dcecef6b5629b71f37502a3bbe30c90d43e9
Nallieheai <nallieh1001@gmail.com>
2019-08-15 21:12:47 +0200

    Routes and Controllers
    Simplified routes and moved all logic to the controllers.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,4 +15,23 @@ class HomeController extends Controller
     {
         return view('contact');
     }
+
+    public function blogPost($id, $welcome = 1)
+    {
+        $pages = [
+            1 => [
+                'title' => 'Hello from page 1'
+            ],
+            2 => [
+                'title' => 'Hello from page 2'
+            ]
+        ];
+    
+        $welcomes = [
+            1 => '<b>Hello</b>',
+            2 => 'Welcome to'
+        ];
+    
+        return view('blog-post', ['data' => $pages[$id], 'welcome' => $welcomes[$welcome]]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -10,34 +10,6 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-// Route::get('/', function () {
-//     return view('home');
-// });
-
-// Route::view('/', 'home')->name('home');
 Route::get('/', 'HomeController@home')->name('home');
-
-// Route::get('/contact', function() {
-//     return view('contact');
-// });
-
 Route::get('/contact', 'HomeController@contact')->name('contact');
-
-Route::get('/blog-post/{id}/{welcome?}', function ($id, $welcome = 1) {
-    $pages = [
-        1 => [
-            'title' => 'Hello from page 1'
-        ],
-        2 => [
-            'title' => 'Hello from page 2'
-        ]
-    ];
-
-    $welcomes = [
-        1 => '<b>Hello</b>',
-        2 => 'Welcome to'
-    ];
-
-    return view('blog-post', ['data' => $pages[$id], 'welcome' => $welcomes[$welcome]]);
-})->name('blog-post');
+Route::get('/blog-post/{id}/{welcome?}', 'HomeController@blogPost')->name('blog-post');
