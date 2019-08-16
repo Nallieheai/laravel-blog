fdcab0eae645f7a7430e3f3c46104600d5dd7c23
Nallieheai <nallieh1001@gmail.com>
2019-08-17 00:01:15 +0200

    Database migrations

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+
 Route::get('/', 'HomeController@home')->name('home');
 Route::get('/contact', 'HomeController@contact')->name('contact');
 Route::get('/blog-post/{id}/{welcome?}', 'HomeController@blogPost')->name('blog-post');
