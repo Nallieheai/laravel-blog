a157c803d675c78b4e795e9ba729fe01dd4d0686
Nallieheai <nallieh1001@gmail.com>
2019-08-18 15:41:55 +0200

    Delete form in the index view and method in the PostController

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -13,6 +13,11 @@
                 <a href="{{ route('posts.show', ['post' => $post->id]) }}">{{ $post->title }}</a>
             </h3>
             <a href="{{ route('posts.edit', ['post' => $post->id]) }}">Edit</a>
+            <form method="POST" action="{{ route('posts.destroy', ['post' => $post->id]) }}">
+                @csrf
+                @method('DELETE')
+                <input type="submit" value="Delete!" />
+            </form>
         </div>
     @empty
         <p>No blog posts yet</p>
