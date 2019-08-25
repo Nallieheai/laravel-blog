2b0b435d9474f0ae01ea58a1cf786b104396db02
Nallieheai <nallieh1001@gmail.com>
2019-08-25 19:49:31 +0200

    Running automated tests against the update URL

--- tests/Feature/PostTest.php
+++ tests/Feature/PostTest.php
@@ -44,4 +44,47 @@ class PostTest extends TestCase
 
         $this->assertEquals(session('status'), 'Blog post was created!');
     }
+
+    public function testStoreFail()
+    {
+        $params = [
+            'title' => 'x',
+            'content' => 'x' 
+        ];
+
+        $this->post('/posts', $params)
+            ->assertStatus(302)
+            ->assertSessionHas('errors');
+
+        $messages = session('errors')->getMessages();
+        // dd($messages->getMessages());
+        $this->assertEquals($messages['title'][0], "The title must be at least 5 characters.");
+        $this->assertEquals($messages['content'][0], "The content must be at least 10 characters.");
+    }
+
+    public function testUpdateValid()
+    {
+        $post = new BlogPost();
+        $post->title = 'New title';
+        $post->content = 'Content of the blog post';
+        $post->save();
+
+        $this->assertDatabaseHas('blog_posts', $post->toArray());
+
+        $params = [
+            'title' => 'A new title',
+            'content' => 'Content that has been updated' 
+        ];
+
+        $this->put("/posts/{$post->id}", $params)
+            ->assertStatus(302)
+            ->assertSessionHas('status');
+
+        $this->assertEquals(session('status'), 'Blog post was updated!');
+        $this->assertDatabaseMissing('blog_posts', $post->toArray());
+        $this->assertDatabaseHas('blog_posts', [
+            'title' => 'A new title',
+            'content' => 'Content that has been updated' 
+        ]);
+    }
 }
