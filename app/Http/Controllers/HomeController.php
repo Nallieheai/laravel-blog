89680ab1a7f124784a52f7d16975b0fd0658a6f6
Nallieheai <nallieh1001@gmail.com>
2019-08-31 12:56:10 +0200

    Display different content for guests and logged in users, Auth methods

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,13 +1,17 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
     public function home()
     {
+        // dd(Auth::user());
+        // dd(Auth::id());
+        // dd(Auth::check());
+        
         return view('home');
     }
 
