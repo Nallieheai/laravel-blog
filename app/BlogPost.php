a340a2c2c81911b7b720b81382e9f4d438dd80ae
Nallieheai <nallieh1001@gmail.com>
2019-09-01 19:28:53 +0200

    Soft deleting & restoring comments + migration

--- app/BlogPost.php
+++ app/BlogPost.php
@@ -16,4 +16,19 @@ class BlogPost extends Model
     {
         return $this->hasMany('App\Comment');
     }
+
+    public static function boot()
+    {
+        // ->forceDelete(); will delete the blog post no matter what.
+
+        parent::boot();
+
+        static::deleting(function (BlogPost $blogPost) {
+            $blogPost->comments()->delete();
+        });
+
+        static::restoring(function (BlogPost $blogPost) {
+            $blogPost->comments()->restore();
+        });
+    }
 }
--- app/Comment.php
+++ app/Comment.php
@@ -3,11 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Comment extends Model
 {
+    use SoftDeletes;
+
     public function blogPost()
     {
+
         // This will look for the blog_post + _id, this is because the name of the function is "blogPost"
         // The function can have a different name but then a second parameter will be needed like so: ->belongsTo('App\BlogPost', 'blog_post_id')
         return $this->belongsTo('App\BlogPost');
