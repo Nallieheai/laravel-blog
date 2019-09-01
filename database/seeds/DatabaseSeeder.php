cbe7fe1cb00e05c09629aaf91958a6d2f587c1d8
Nallieheai <nallieh1001@gmail.com>
2019-09-01 09:57:44 +0200

    Database seeding with model factory

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,13 +12,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
-            'name' => "John Doe",
-            'email' => "john@laravel.test",
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
-            'remember_token' => Str::random(10)
-        ]);
         // $this->call(UsersTableSeeder::class);
+
+        factory(App\User::class)->states('john-doe')->create();
+        factory(App\User::class, 20)->create();
     }
 }
