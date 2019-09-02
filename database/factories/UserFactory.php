b393d3347a1a6451d75555e400e599a95d322e02
Nallieheai <nallieh1001@gmail.com>
2019-09-02 12:56:20 +0200

    Admin migration for users and fixed typ for SQLite

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,6 +29,7 @@ $factory->define(User::class, function (Faker $faker) {
 $factory->state(App\User::class, "john-doe", function (Faker $faker) {
     return [
         'name' => "John Doe",
-        'email' => "john@laravel.test"
+        'email' => "john@laravel.test",
+        'is_admin' => true
     ];
 });
