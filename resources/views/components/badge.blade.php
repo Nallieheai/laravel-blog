23f65a4bde3826a7caeb9180c0c12f541a0adc61
Nallieheai <nallieh1001@gmail.com>
2019-09-09 23:32:34 +0200

    Conditional argument & cleaner HTML

--- resources/views/components/badge.blade.php
+++ resources/views/components/badge.blade.php
@@ -1,1 +1,3 @@
-<div class="badge badge-{{ $type ?? 'success' }}">{{ $slot }}</div>
+@if (!isset($show) || $show)
+<span class="badge badge-{{ $type ?? 'success' }}">{{ $slot }}</span>
+@endif
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,26 +1,28 @@
 @extends('layout')
 
 @section('content')
-    <h1>{{ $post->title }}</h1>
-    <p>{{ $post->content }}</p>
+<h1>
+    {{ $post->title }} 
+    @badge(['type'=> 'primary', 'show' => now()->diffInMinutes($post->created_at) < 30])
+        New post!
+    @endbadge
+</h1>
 
-    <p class="text-muted">{{ $post->created_at->diffForHumans() }}  by {{ $post->user->name }}</p>
+<p>{{ $post->content }}</p>
 
-    @if ((new Carbon\Carbon())->diffInMinutes($post->created_at) < 30)
-        @badge(['type' => 'primary'])
-            New post!
-        @endbadge
-    @endif
+<p class="text-muted">{{ $post->created_at->diffForHumans() }} by {{ $post->user->name }}</p>
 
-    <h4>Comments</h4>
-    @forelse ($post->comments as $comment)
-        <div class="card">
-            <div class="card-body">
-                <p>{{ $comment->content }}</p>
-                <em class="text-muted">Added {{ $comment->created_at->diffForHumans() }}</em>
-            </div>
-        </div>
-    @empty
-        <p>No comments yet!</p>
-    @endforelse
+
+
+<h4>Comments</h4>
+@forelse ($post->comments as $comment)
+<div class="card">
+    <div class="card-body">
+        <p>{{ $comment->content }}</p>
+        <em class="text-muted">Added {{ $comment->created_at->diffForHumans() }}</em>
+    </div>
+</div>
+@empty
+<p>No comments yet!</p>
+@endforelse
 @endsection
