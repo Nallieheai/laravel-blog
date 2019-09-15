284f40c6fe08eb6a5500f7a5967dd3d1cb4eeef8
Nallieheai <nallieh1001@gmail.com>
2019-09-15 18:39:58 +0200

    Reset all of the Cache using tags and flush

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -37,15 +37,15 @@ class PostController extends Controller
         // Time in minutes
         $cachedTime = 60;
 
-        $mostCommented = Cache::remember('blog-post-commented', $cachedTime, function () {
+        $mostCommented = Cache::tags(['blog-post'])->remember('blog-post-commented', $cachedTime, function () {
             return BlogPost::mostCommented()->take(5)->get();
         });
 
-        $mostActive = Cache::remember('users-most-active', $cachedTime, function () {
+        $mostActive = Cache::tags(['blog-post'])->remember('users-most-active', $cachedTime, function () {
             return User::withMostBlogPosts()->take(5)->get();
         });
 
-        $mostActiveLastMonth = Cache::remember('users-most-active-last-month', $cachedTime, function () {
+        $mostActiveLastMonth = Cache::tags(['blog-post'])->remember('users-most-active-last-month', $cachedTime, function () {
             return User::withMostBlogPostsLastMonth()->take(5)->get();
         });
         
@@ -73,7 +73,7 @@ class PostController extends Controller
         //     return $query->latest();
         // }])->findOrFail($id)]);
         
-        $blogPost = Cache::remember("blog-post-{$id}", 60, function() use($id) {
+        $blogPost = Cache::tags(['blog-post'])->remember("blog-post-{$id}", 60, function() use($id) {
             return BlogPost::with('comments')->findOrFail($id);
         });
 
@@ -81,7 +81,7 @@ class PostController extends Controller
         $counterKey = "blog-post-{$id}-counter";
         $usersKey = "blog-post-{$id}-users";
 
-        $users = Cache::get($usersKey, []);
+        $users = Cache::tags(['blog-post'])->get($usersKey, []);
         $usersUpdate = [];
         $difference = 0;
         $now = now();
@@ -97,14 +97,14 @@ class PostController extends Controller
             $difference++;
 
         $usersUpdate[$sessionId] = $now;
-        Cache::forever($usersKey, $usersUpdate);
+        Cache::tags(['blog-post'])->forever($usersKey, $usersUpdate);
 
-        if (!Cache::has($counterKey)) 
-            Cache::forever($counterKey, 1);
+        if (!Cache::tags(['blog-post'])->has($counterKey)) 
+            Cache::tags(['blog-post'])->forever($counterKey, 1);
         else 
-            Cache::increment($counterKey, $difference);
+            Cache::tags(['blog-post'])->increment($counterKey, $difference);
     
-        $counter = Cache::get($counterKey);
+        $counter = Cache::tags(['blog-post'])->get($counterKey);
 
         return view('posts.show', [
             'post' => $blogPost,
